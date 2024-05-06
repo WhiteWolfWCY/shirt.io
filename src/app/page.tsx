72c2ed591d887f4e0244f5b7b62fab1280064637
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-05-06 16:48:27 +0200

    Animated reviews section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Icons } from "@/components/icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Reviews } from "@/components/Reviews";
 import Shirt from "@/components/Shirt";
 import { Check, Star } from "lucide-react";
 
@@ -105,6 +107,97 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+      </section>
+
+      {/* Value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-7 text-orange-500" />
+              </span>{" "}
+              say?
+            </h2>
+            <img src="/logo.png" className="w-40 order-0 lg:order-2" />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The shirt looks amazing even after almost two years of
+                  wearing it. The image is clear and{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    not a single scratch
+                  </span>{" "}
+                  on it. I even got a compliment on the look of the shirt!"
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-orange-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+                <Star className="h-5 w-5 text-orange-600 fill-orange-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I usually pay attention to the materials that the clothes I
+                  buy are made of. I was kindly suprised when I got the shirt.
+                  From the first moment I felt the quality of it.{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    The graphics are so precise and amazing.
+                  </span>{" "}
+                  Love it!"
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Matt</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-orange-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   );
