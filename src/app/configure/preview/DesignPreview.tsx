e421c53426526dc22b90fc7d79b4c628257166d4
WhiteWolfWCY <mateuszmartyna44@gmail.com>
2024-05-10 22:40:33 +0200

    Preview page in configurator

---
+++ src/app/configure/preview/DesignPreview.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import Shirt from "@/components/Shirt";
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
+import { cn, formatPrice } from "@/lib/utils";
+import { COLORS, FINISHES, SIZES } from "@/validators/option-validator";
+import { Configuration } from "@prisma/client";
+import { Check } from "lucide-react";
+import { useEffect, useState } from "react";
+import Confetti from "react-dom-confetti";
+
+const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
+  const [showConfetti, setShowConfetti] = useState<boolean>(false);
+
+  useEffect(() => setShowConfetti(true), []);
+
+  const { color, size, finish, material } = configuration;
+  const { label: sizeLabel } = SIZES.options.find(
+    ({ value }) => value === size
+  )!;
+  const tw = COLORS.find(
+    (supportedColor) => supportedColor.value === color
+  )?.tw;
+
+  return (
+    <>
+      <div
+        className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center"
+        aria-hidden="true"
+      >
+        <Confetti
+          active={showConfetti}
+          config={{
+            elementCount: 200,
+            spread: 90,
+          }}
+        />
+      </div>
+
+      <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
+        <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
+          <Shirt
+            imgSrc={configuration.croppedImgUrl!}
+            className={cn(`bg-${tw}`)}
+          />
+        </div>
+
+        <div className="mt-6 sm:col-span-9 sm:mt-0 md:row-end-1">
+          <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+            Your {sizeLabel} Shirt
+          </h3>
+          <div className="mt-3 flex items-center gap-1.5 text-base">
+            <Check className="h-4 w-4 text-orange-500" />
+            In stock and ready to ship
+          </div>
+        </div>
+
+        <div className="sm:col-span-12 md:col-span-9 text-base">
+          <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+            <div>
+              <p className="font-medium text-zinc-950">Highlights</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>Breathable and light</li>
+                <li>Pachaging made from recycle materials</li>
+                <li>5 year print warranty</li>
+              </ol>
+            </div>
+
+            <div>
+              <p className="font-medium text-zinc-950">Materials</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>High quality, durable material</li>
+                <li>Anti-scratch print</li>
+              </ol>
+            </div>
+          </div>
+
+          <div className="mt-8">
+            <div className="bg-gray-50 p-6 sm:rounded-lg sm:p-8">
+              <div className="flow-root text-sm">
+                <div className="flex items-center justify-between py-1 mt-2">
+                  <p className="text-gray-600">Base price</p>
+                  <p className="font-medium text-gray-900">
+                    {formatPrice(BASE_PRICE / 100)}
+                  </p>
+                </div>
+
+                {finish === "textured" ? (
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">Textured finish</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(PRODUCT_PRICES.finish.textured / 100)}
+                    </p>
+                  </div>
+                ) : null}
+
+                {material === "cotton" || "triblend" ? (
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">{
+                      material === "cotton" ? "Cotton material" : "Tri-Blend Material"
+                    }</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(
+                        (material === "cotton"
+                          ? PRODUCT_PRICES.material.cotton
+                          : PRODUCT_PRICES.material.triblend) / 100
+                      )}
+                    </p>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DesignPreview;
