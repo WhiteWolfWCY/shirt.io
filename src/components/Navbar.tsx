0ba251bb78b321933ca9e4e30e762135affa8c5b
WhiteWolfWCY <mateuszmartyna44@gmail.com>
2024-05-11 21:33:33 +0200

    Admin dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = async () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
+                    href="/dashboard"
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
